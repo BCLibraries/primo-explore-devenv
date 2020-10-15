23ad0831c45be03938901ca969c6b31962670cfa
Luke Gaudreau <gaudreal@bc.edu>
2020-10-15 12:08:09 -0400

    Switch to nightwatch
    We already have it set up on lib-ci

--- gulp/tasks/11-tests.js
+++ gulp/tasks/11-tests.js
@@ -4,35 +4,21 @@ const wrap = require("gulp-wrap");
 const templateCache = require('gulp-angular-templatecache');
 const config = require('../config.js');
 const fs = require('fs');
-const protractor = require("gulp-protractor").protractor;
+const nightwatch = require("gulp-nightwatch");
 
 let buildParams = config.buildParams;
  
-// Download and update the selenium driver
-var webdriver_update = require('gulp-protractor').webdriver_update_specific;
- 
-// Downloads the selenium webdriver - stupid solution to pass extra args like ignore_ssl
-gulp.task('webdriver_update', webdriver_update({
-    webdriverManagerArgs: ['--ignore_ssl']
-}));
-
-var webdriver_standalone = require("gulp-protractor").webdriver_standalone;
-gulp.task('webdriver_standalone', webdriver_standalone);
-
-
-function runTestsWithProtractor(){
-    return gulp.src(buildParams.viewTestsDir() + '/*-spec.js')
-    .pipe(protractor({
-      configFile: buildParams.viewTestsDir() + '/protractor.config.js',
-        args: [
-            '--baseUrl', 'http://localhost:8003/primo-explore/search?vid=bclib_new',
-            '--suite', 'production',
-            // '--params.environment', 'test'
+function runTestsWithNightwatch(){
+    return gulp.src('gulpfile.js')
+    .pipe(nightwatch({
+      configFile: buildParams.viewTestsDir() + '/nightwatch.conf.js',
+        cliArgs: [
+            '--tag', 'production',
         ]
     }))
   .on('error', function(e) { throw e; });
 }
 
 gulp.task('run-tests', gulp.series('run', (cb) => {
-  runTestsWithProtractor().on('end', cb);
+  runTestsWithNightwatch().on('end', cb);
 }));
